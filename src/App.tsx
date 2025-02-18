8d1abb625956ec7c2ca6b168b54c073ed23cff00
hyna <bylo@duck.com>
2025-02-18 20:26:51 +0100

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <a href='#'>Home</a>
+      <a href='#' className='text-red-400'>Home</a>
       <a href='#post'>Articles</a>
       <a href='#contact'>Contact</a>
     </>
