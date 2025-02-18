192ed5f4598a1a9081c3ec656f331490e8093518
hyna <bylo@duck.com>
2025-02-18 20:20:18 +0100

    init blog-app  project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <a href='#'>Home</a>
+      <a href='#post'>Articles</a>
+      <a href='#contact'>Contact</a>
+    </>
+  )
+}
+
+export default App
