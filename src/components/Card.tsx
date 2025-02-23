48f5bfadba2cfdc77a3547657661af5beca230cc
hyna <bylo@duck.com>
2025-02-23 14:51:30 +0100

    feat : add responsive Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,19 +25,20 @@ const PostCard = ({
 }: Cardprops) => {
   console.log(image, title, description, href, buttonLabel);
   return (
-    <Card className="w-[350px] mb-6">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <img src={image} />
-        {/* <p>Card Content</p> */}
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">{buttonLabel}</Button>
-      </CardFooter>
-    </Card>
+    <Card className="w-[350px] flex flex-col overflow-hidden border shadow-md">
+    <CardHeader>
+      <CardTitle className="truncate">{title}</CardTitle>
+      <CardDescription className="line-clamp-2 md:line-clamp-none">{description}</CardDescription>
+    </CardHeader>
+    <CardContent className="flex-grow">
+      <img src={image} className="w-full h-40 object-cover" alt="Post" />
+      <p className="text-sm line-clamp-2">Card Content</p>
+    </CardContent>
+    <CardFooter className="mt-auto">
+      <Button className="w-full bg-sky-800 hover:bg-amber-600">{buttonLabel}</Button>
+    </CardFooter>
+  </Card>
+  
   );
 };
 export default PostCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,27 +8,25 @@ const Home = () => {
     "https://jsonplaceholder.typicode.com/posts?_limit=10"
   );
   console.log("data", data, loading, error);
+
   return (
     <>
       {loading && <Spinner />}
       {error && <Alert message={error.message} />}
-      <h1>Mon Blog</h1>
-
-      <div className="flex">
-        {data && (
-          <div>
-            {data.map((post) => (
-              <PostCard
-                key={post.id}
-                title={post.title}
-                description={post.body}
-                href={`#post:${post.id}`}
-                buttonLabel="Voir l'article"
-              />
-            ))}
-          </div>
-        )}
-      </div>
+      {data && (
+        <div className="flex flex-wrap justify-center gap-3 p-3">
+          {/* <h1>Mon Blog</h1> */}
+          {data.map((post) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              description={post.body}
+              href={`#post:${post.id}`}
+              buttonLabel="Voir l'article"
+            />
+          ))}
+        </div>
+      )}
     </>
   );
 };
